93521b9c6cc9c27d7a70450fbd7f33d6210e08cf
ArRosid <arrosid@ArRosids-MacBook-Air.local>
2022-07-22 05:39:38 +0700

    setting database to use mysql

--- teman_kelas/settings.py
+++ teman_kelas/settings.py
@@ -84,8 +84,15 @@ WSGI_APPLICATION = 'teman_kelas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env.str("DATABASE_NAME"),
+        "USER": env.str("DATABASE_USER"),
+        "PASSWORD": env.str("DATABASE_PASSWORD"),
+        "HOST": env.str("DATABASE_HOST"),
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }
     }
 }
 
